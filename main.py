feeeb58dc8d2b8f1ad0190efa4b386070d92b722
JoelAllen1 <124019479+JoelAllen1@users.noreply.github.com>
2024-07-08 20:22:35 +1000

    V 0.12.0 (Added a help screen, added functionality to the next button, added a reset progress button, designed 3 levels, fixed bug in the generator subroutine, fixed bug with drag and drop functionality and other minor changes)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame as py
 import pathlib
 import button
 import ast
+import sys
 
 py.init()
 
@@ -42,7 +43,7 @@ cell_images = {
     12: py.image.load(IMAGE_DIR / 'GeneratorRight.png').convert_alpha(),
     13: py.image.load(IMAGE_DIR / 'RotatorClockwise.png').convert_alpha(),
     14: py.image.load(IMAGE_DIR / 'RotatorAnticlockwise.png').convert_alpha(),
-    15: py.image.load(IMAGE_DIR / 'Badguy.png').convert_alpha()
+    15: py.image.load(IMAGE_DIR / 'Enemy.png').convert_alpha()
 }
 
 
@@ -54,37 +55,47 @@ back_img = py.image.load(IMAGE_DIR / 'Back.png').convert_alpha()
 next_img = py.image.load(IMAGE_DIR / 'Next.png').convert_alpha()
 play_img = py.image.load(IMAGE_DIR / 'Play.png').convert_alpha()
 quit_img = py.image.load(IMAGE_DIR / 'Quit.png').convert_alpha()
+help_img = py.image.load(IMAGE_DIR / 'Help.png').convert_alpha()
 level1_img = py.image.load(IMAGE_DIR / 'Level1.png').convert_alpha()
 level2_img = py.image.load(IMAGE_DIR / 'Level2.png').convert_alpha()
 level3_img = py.image.load(IMAGE_DIR / 'Level3.png').convert_alpha()
 completedlevel1_img = py.image.load(IMAGE_DIR / 'completedlevel1.png').convert_alpha()
 completedlevel2_img = py.image.load(IMAGE_DIR / 'completedlevel2.png').convert_alpha()
 completedlevel3_img = py.image.load(IMAGE_DIR / 'completedlevel3.png').convert_alpha()
+resetprogress_img = py.image.load(IMAGE_DIR / 'Resetprogress.png').convert_alpha()
 title_img = py.transform.scale(py.image.load(IMAGE_DIR / 'Title.png').convert_alpha(), (536, 56))
 levels_img = py.transform.scale(py.image.load(IMAGE_DIR / 'Levels.png').convert_alpha(), (280, 56))
 lock_img = py.transform.scale(py.image.load(IMAGE_DIR / 'lock.png').convert_alpha(), (90, 90))
+helptitle_img = py.transform.scale(py.image.load(IMAGE_DIR / 'Helptitle.png').convert_alpha(), (184, 56))
+
+py.display.set_caption('Cell Machine')
+py.display.set_icon(cell_images[5])
+
 
 #Game screen buttons
-start_button = button.Button(50, 676, start_img, 5)
-stop_button = button.Button(50, 676, stop_img, 5)
-step_button = button.Button(150, 676, step_img, 5)
-restart_button = button.Button(250, 676, restart_img, 5)
-back_button = button.Button(50, 50, back_img, 3)
-next_button = button.Button(600, 676, next_img, 5)
+start_button = button.Button(50, 701, start_img, 5)
+stop_button = button.Button(50, 701, stop_img, 5)
+step_button = button.Button(150, 701, step_img, 5)
+restart_button = button.Button(250, 701, restart_img, 5)
+back_button = button.Button(50, 25, back_img, 3)
+next_button = button.Button(600, 701, next_img, 5)
 
+#Level screen buttons
 level1_button = button.Button(155, 355, level1_img, 6)
 level2_button = button.Button(355, 355, level2_img, 6)
 level3_button = button.Button(555, 355, level3_img, 6)
 completedlevel1_button = button.Button(155, 355, completedlevel1_img, 6)
 completedlevel2_button = button.Button(355, 355, completedlevel2_img, 6)
 completedlevel3_button = button.Button(555, 355, completedlevel3_img, 6)
+resetprogress_button = button.Button(347, 700, resetprogress_img, 2)
 
 #Menu screen buttons
 play_button = button.Button(284, 250, play_img, 8)
-quit_button = button.Button(342, 375, quit_img, 4)
+quit_button = button.Button(342, 449, quit_img, 4)
+help_button = button.Button(342, 375, help_img, 4)
 
 
-def is_within_valid_area(col, row, level):
+def is_within_valid_area(col, row, level): # Used in functionality of editing the grid to determine the area that can be edited
     valid_area_top_left = load_value('levels.txt', level)[0]
     valid_area_bottom_right = load_value('levels.txt', level)[1]
     return (
@@ -93,7 +104,7 @@ def is_within_valid_area(col, row, level):
     )
 
 
-def draw_grid(level, dragging=False, dragged_cell=None, mouse_pos=None):
+def draw_grid(level, dragging=False, dragged_cell=None, mouse_pos=None): #Draws the entire game screen except for the buttons
     global grid, TILE_SIZE, GRID_HEIGHT, GRID_WIDTH
     valid_area_top_left = load_value('levels.txt', level)[0]
     valid_area_bottom_right = load_value('levels.txt', level)[1]
@@ -122,7 +133,7 @@ def draw_grid(level, dragging=False, dragged_cell=None, mouse_pos=None):
         screen.blit(dragged_cell, (mouse_pos[0] - TILE_SIZE // 2, mouse_pos[1] - TILE_SIZE // 2))
     
 
-def cell_properties(cell_id):
+def cell_properties(cell_id): #Stores the properties of the different cell types
     if cell_id == 0:  # Empty Cell
         return {'movable_x': True, 'movable_y': True}
     elif cell_id == 1:  # Wall Cell
@@ -139,9 +150,9 @@ def cell_properties(cell_id):
         return {}
 
 
-def adjust_grid():
+def adjust_grid(): #Handles the execution of the functionality to update the grid
     global grid
-    new_grid = [row[:] for row in grid]  #to store the locations of the bad guy cells
+    new_grid = [row[:] for row in grid]  #to store the locations of the enemy cells
 
     for ID in range(5, 15):
         if ID >= 5 and ID <= 8:
@@ -151,12 +162,12 @@ def adjust_grid():
         elif ID == 13 or ID == 14:
             grid = rotator(ID)
     
-    grid = bad_guy(new_grid)
+    grid = enemy(new_grid)
         
 
     return grid
 
-def pusher(ID): 
+def pusher(ID): #Functionality for pusher cells
     global grid
     temp_grid = [row[:] for row in grid]  # Create a new grid to avoid modifying the original during iteration
 
@@ -170,20 +181,20 @@ def pusher(ID):
     sy, sx, opp_id, movable = directions[ID]
     
     # Determine the direction of iteration based on direction of pusher cell
-    if sy == -1 or sx == -1:  # pusher up/left
+    if sy == -1 or sx == -1:
         row_range = range(GRID_HEIGHT)
         col_range = range(GRID_WIDTH)
-    elif sy == 1:  # pusher down
+    elif sy == 1:
         row_range = range(GRID_HEIGHT - 1, -1, -1)
         col_range = range(GRID_WIDTH)
-    elif sx == 1:  # pusher right
+    elif sx == 1:
         row_range = range(GRID_HEIGHT)
         col_range = range(GRID_WIDTH - 1, -1, -1)
 
     for row in row_range:
         for col in col_range:
             cell_id = grid[row][col]
-            if cell_id == ID:  # Check the grid for the specified ID
+            if cell_id == ID:
                 run = True
                 y = 0
                 list_cell = []
@@ -206,9 +217,9 @@ def pusher(ID):
                     y -= 1
                     temp_grid[row][col] = 0
 
-    return temp_grid  # Return the modified grid
+    return temp_grid
 
-def generator(ID):
+def generator(ID): #Functionality for generator cells
     global grid
     temp_grid = [row[:] for row in grid]  # Create a new grid to avoid modifying the original during iteration
 
@@ -227,7 +238,7 @@ def generator(ID):
 
     for row in range(GRID_HEIGHT):
         for col in range(GRID_WIDTH):
-            if grid[row][col] == ID and grid[row - sy][col - sx] != 0:  # Check against the specified ID
+            if grid[row][col] == ID and (grid[row - sy][col - sx] > 1):
                 run = True
                 generate = True
                 y = 1
@@ -254,9 +265,9 @@ def generator(ID):
                 if generate:  # Set the cell right in front of the generator to the cell behind
                     temp_grid[row + sy][col + sx] = grid[row - sy][col - sx]
 
-    return temp_grid  # Return the modified grid
+    return temp_grid  
 
-def rotator(ID):
+def rotator(ID): #Functionality for rotator cells
     global grid
     temp_grid = [row[:] for row in grid]  # Create a new grid to avoid modifying the original during iteration
 
@@ -266,7 +277,7 @@ def rotator(ID):
                     2:2, # Blank
                     13:13, # Rotator
                     14:14, # Rotator
-                    15:15, #Badguy
+                    15:15, # Enemy
                     
                     3: 4, # blank x to y
                     4: 3, # blank y to x
@@ -288,7 +299,7 @@ def rotator(ID):
                     2:2, # Blank
                     13:13, # Rotator
                     14:14, # Rotator
-                    15:15, # Badguy
+                    15:15, # Enemy
                     
                     3: 4, # blank x to y
                     4: 3, # blank y to x
@@ -312,14 +323,14 @@ def rotator(ID):
     for row in range(GRID_HEIGHT):
         for col in range(GRID_WIDTH):
             cell_id = grid[row][col]
-            if cell_id == ID:  # Check against the specified ID
+            if cell_id == ID:
                 temp_grid[row - 1][col] = rotation_mapping[temp_grid[row - 1][col]]
                 temp_grid[row + 1][col] = rotation_mapping[temp_grid[row + 1][col]]
                 temp_grid[row][col - 1] = rotation_mapping[temp_grid[row][col - 1]]
                 temp_grid[row][col + 1] = rotation_mapping[temp_grid[row][col + 1]]
     return temp_grid
 
-def bad_guy(new_grid):
+def enemy(new_grid): #Functionality for enemy cells
 
     for row in range(GRID_HEIGHT):
         for col in range(GRID_WIDTH):
@@ -330,19 +341,25 @@ def bad_guy(new_grid):
     return grid
 
 
-def save_value(input_value):
+def save_value(input_value): #Handles storeing player progress
     with open('progress.txt', 'w') as f:
         f.write(input_value)
 
-def load_value(file_name, index):
+def load_value(file_name, index): #Handles retrieveing player progress and the levels
     with open(file_name, 'r') as f:
         read = ast.literal_eval(f.read())
         if index != None:
             read = read[index]
     return read
 
+def draw_text(text, y): #Handles the display of text on the help screen
+    font = py.font.SysFont('Pixeloid Sans', 20)
+    img = font.render(text, True, (255, 255, 255))
+    text_rect = img.get_rect(center=(400, y))
+    screen.blit(img, text_rect)
 
-def game(level):
+
+def game(level): #The game window where all the gameplay happens
     global grid, dragging, dragged_cell, original_pos, TILE_SIZE, GRID_HEIGHT, GRID_WIDTH, WIDTH, HEIGHT
     window_running = True
     sim_running = False
@@ -367,7 +384,7 @@ def game(level):
 
         screen.fill(BLACK)
         draw_grid(level, dragging, dragged_cell, py.mouse.get_pos())
-        
+        #Drawing the buttons to the screen and handling their functionality
         if sim_running:
             if stop_button.draw(screen):
                 sim_running = False
@@ -381,10 +398,10 @@ def game(level):
         
         if step_button.draw(screen):
             sim_running = False
-            grid = adjust_grid()
             if editing:
                 edited_grid = [row[:] for row in grid]
                 editing = False
+            grid = adjust_grid()
         if not editing:
             if restart_button.draw(screen):
                 sim_running = False
@@ -396,122 +413,164 @@ def game(level):
         if not any(15 in row for row in grid):
             progress = load_value('progress.txt', None)
             progress[level] = 2
-            if progress[level + 1] == 0:
+            if level + 1 in progress and progress[level + 1] == 0:
                 progress[level + 1] = 1
             save_value(str(progress))
             if next_button.draw(screen):
-                return
+                return level + 1
 
         py.display.update()
 
         for event in py.event.get():
             if event.type == py.QUIT:
                 window_running = False
-            
-            if event.type == py.MOUSEBUTTONDOWN and event.button == 1:  # Check for left mouse button
+                py.quit()
+                sys.exit()
+            #Functionality for editing the grid
+            if event.type == py.MOUSEBUTTONDOWN and event.button == 1:
                 mouse_x, mouse_y = event.pos
                 col = mouse_x // TILE_SIZE
                 row = mouse_y // TILE_SIZE
                 
                 if 0 <= col < GRID_WIDTH and 0 <= row < GRID_HEIGHT and is_within_valid_area(col, row, level) and editing:
                     cell_id = grid[row][col]
-                    if cell_id != 0:  # Only start dragging if the cell is not empty
+                    if cell_id != 0:
                         dragging = True
                         dragged_cell = cell_images[cell_id]
                         original_pos = (col, row)
-                        grid[row][col] = 0  # Temporarily clear the cell in the grid
+                        grid[row][col] = 0 
 
-            if event.type == py.MOUSEBUTTONUP and event.button == 1:  # Check for left mouse button
+            if event.type == py.MOUSEBUTTONUP and event.button == 1:
                 if dragging:
                     mouse_x, mouse_y = event.pos
                     col = mouse_x // TILE_SIZE
                     row = mouse_y // TILE_SIZE
                     
                     if 0 <= col < GRID_WIDTH and 0 <= row < GRID_HEIGHT and is_within_valid_area(col, row, level) and editing:
-                        # Place the cell in the new position
                         grid[original_pos[1]][original_pos[0]], grid[row][col] = grid[row][col], cell_id
                     else:
-                        # Revert the cell to its original position
-                        grid[original_pos[1]][original_pos[0]] = list(cell_images.keys())[list(cell_images.values()).index(dragged_cell)]
+                        grid[original_pos[1]][original_pos[0]] = cell_id
 
                     dragging = False
                     dragged_cell = None
                     original_pos = None
-                
-            if event.type == py.KEYDOWN:
-                pass
 
-    py.quit()
+            if event.type == py.MOUSEMOTION and dragging:
+                mouse_x, mouse_y = event.pos
+                col = mouse_x // TILE_SIZE
+                row = mouse_y // TILE_SIZE
 
-def level_select():
+                if not (0 <= col < GRID_WIDTH and 0 <= row < GRID_HEIGHT and is_within_valid_area(col, row, level)):
+                    grid[original_pos[1]][original_pos[0]] = cell_id
+                    dragging = False
+                    dragged_cell = None
+                    original_pos = None
+
+
+
+def level_select(): #The level select screen
     window_running = True
+    next = 0
     
     while window_running:
         clock.tick(FPS)
-        levels = load_value('progress.txt', None)
+        levels = load_value('progress.txt', None) #Retrieves which levels are locked, unlocked or completed
         
         screen.fill(BLACK)
-        screen.blit(levels_img, (260, 50))
+        screen.blit(levels_img, (260, 25))
+        #Drawing the buttons to the screen and handling their functionality
         if back_button.draw(screen):
             return
         if levels[1] == 1:
             if level1_button.draw(screen):
-                game(1)
+                next = 0
+                next = game(1)
         else:
             if completedlevel1_button.draw(screen):
-                game(1)
+                next = 0
+                next = game(1)
         if levels[2] == 0:
             screen.blit(lock_img, (355, 355))
         elif levels[2] == 1:
-            if level2_button.draw(screen):
-                game(2)
+            if level2_button.draw(screen) or next == 2:
+                next = 0
+                next = game(2)
         else:
-            if completedlevel2_button.draw(screen):
-                game(2)
+            if completedlevel2_button.draw(screen) or next == 2:
+                next = 0
+                next = game(2)
         if levels[3] == 0:
             screen.blit(lock_img, (555, 355))
         elif levels[3] == 1:
-            if level3_button.draw(screen):
+            if level3_button.draw(screen) or next == 3:
+                next = 0
                 game(3)
         else:
-            if completedlevel3_button.draw(screen):
+            if completedlevel3_button.draw(screen) or next == 3:
+                next = 0
                 game(3)
+        if resetprogress_button.draw(screen):
+            save_value("{1: 1, 2: 0, 3: 0}")
         py.display.update()
         
         for event in py.event.get():
             if event.type == py.QUIT:
                 window_running = False
+                py.quit()
+                sys.exit()
 
-            if event.type == py.KEYDOWN:
-                pass
+def help(): #The help screen
+    window_running = True
+    while window_running:
+        clock.tick(FPS)
 
-    py.quit()
+        screen.fill(BLACK)
+        screen.blit(helptitle_img, (308, 25))
+        draw_text('Move cells within the highlighted area and then click the play button.', 150)
+        draw_text('Once the play button is clicked you cannot move cells yourself.', 175)
+        draw_text('The goal is to remove all enemy cells. This is done by moving another', 200)
+        draw_text("cell into the same space as an enemy cell. If you don't manage to", 225)
+        draw_text('remove all of the enemy cells, you can click the reset button to try', 250)
+        draw_text('again. The step button steps through the simulation one step at a time.', 275)
+        draw_text('Blue cells push things in front of them in the direction of their arrow.', 350)
+        draw_text('Green cells duplicate the cell behind it and place it in front.', 400)
+        draw_text('Red cells rotate cells surrounding them in the direction of the arrow.', 450)
+        draw_text('Yellow cells can only be moved in the direction of their arrow.', 500)
+        if back_button.draw(screen): #Draws the back button and handles its functionality
+            return
+        py.display.update()
 
-def menu():
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                window_running = False
+                py.quit()
+                sys.exit()
+
+
+def menu(): #The menu screen
     window_running = True
     while window_running:
         clock.tick(FPS)
         
         screen.fill(BLACK)
         screen.blit(title_img, (132, 100))
+        #Drawing the buttons to the screen and handling their functionality
         if play_button.draw(screen):
             level_select()
         if quit_button.draw(screen):
             window_running = False
+        if help_button.draw(screen):
+            help()
         py.display.update()
 
         for event in py.event.get():
             if event.type == py.QUIT:
                 window_running = False
-
-            if event.type == py.KEYDOWN:
-                pass
-    py.quit()
+                py.quit()
+                sys.exit()
         
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #runs the menu subroutine on opening
     menu()
-
-py.quit
